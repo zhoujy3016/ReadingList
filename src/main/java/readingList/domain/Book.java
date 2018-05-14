a794fd018d5d2cf4f0dae792d85d2b10035068a5
zhoujy <zhoujy@zhoujy-PC>
2018-05-14 10:12:37 +0800

    删除包

--- src/main/java/readingList/domain/Book.java
+++ src/main/java/readingList/domain/Book.java
@@ -1,20 +1,15 @@
 package readingList.domain;
 
 import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 import javax.validation.constraints.NotEmpty;
-
-import org.springframework.web.bind.annotation.Mapping;
-
 
 @Entity
 @Table(name="book")
