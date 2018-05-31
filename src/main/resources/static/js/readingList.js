07e70c11415cc819f215792f823f9aca45bedd27
zhoujy <zhoujy@zhoujy-PC>
2018-05-31 09:02:47 +0800

    加个下拉菜单

--- src/main/resources/static/js/readingList.js
+++ src/main/resources/static/js/readingList.js
@@ -1,10 +1,26 @@
 $(function () {
     $.ajax({
         type: "POST",
-        url: "/getDictCache/mz,sex",
+        url: "/getDictCache/pub",
         contentType: "application/json",
         success: function(r){
-        	alert(JSON.stringify(r));
+        	var options = '';
+        	for(i = 0; i < r.pub.length; i++) {
+        		options += '<option>'+ r.pub[i].value +'</option>'
+        	}
+        	$("#sel").append(options);
         }
     });
+});
+
+
+
+var vm = new Vue({
+	el:'#rrapp',
+	data:{
+		dictPub:{},
+		dictArea:{}
+	},
+	methods: {
+	}
 });
